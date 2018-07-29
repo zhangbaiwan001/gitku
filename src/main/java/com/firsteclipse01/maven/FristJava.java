11546f46e211500690271a9e79a5cafeecff52c6
zhangbaiwan001 <809322536@qq.com>
2018-07-29 14:17:24 +0800

    vv

--- src/main/java/com/firsteclipse01/maven/FristJava.java
+++ src/main/java/com/firsteclipse01/maven/FristJava.java
@@ -2,7 +2,7 @@ package com.firsteclipse01.maven;
 
 public class FristJava {
 	public String getJ(String name) {
-		String va = "hello" + name;
+		String va = name+"...ccccc";
 		System.out.println(va);
 		return va;
 	}
