53f0ece1367e9480043c5c6fba0b1d91198c6259
zhangbaiwan001 <809322536@qq.com>
2018-07-29 16:25:44 +0800

    vv
    Change-Id: I97576da435a685987a6ae53e701cca186acde7a4
    Signed-off-by: zhangbaiwan001 <809322536@qq.com>

--- src/main/java/com/firsteclipse01/maven/FristJava.java
+++ src/main/java/com/firsteclipse01/maven/FristJava.java
@@ -2,7 +2,7 @@ package com.firsteclipse01.maven;
 
 public class FristJava {
 	public String getJ(String name) {
-		String va = name+"...cccccdD";
+		String va = name+"...cccccdDFGFGFDGbgbgbvgB";
 		System.out.println(va);
 		return va;
 	}
